745c6549cf75ecfa08b417be6720c1a58b0b0729
Azaria Kelman <askelman5@gmail.com>
2025-07-18 18:11:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('start-btn').addEventListener('click', () => {
+    console.log('Game started!');
+});
+
+document.getElementById('help-btn').addEventListener('click', () => {
+    alert('This is a memory game inspired by the book "Moonwalking with Einstein". The goal is to build your own memory palace. Have fun!');
+}); 
